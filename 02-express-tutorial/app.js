31b1d534cc3764efb843b4711f87c029ca5abc15
sourabh <sourabhchavan20.sc@gmail.com>
2022-08-24 00:17:48 +0530

    about methods, postman, router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,18 +1,27 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
 
-app.use(express.static("./public"));
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+  const people = require('./routes/people')
+// static assets
+app.use(express.static("./methods-public"));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+
+app.use('/api/people',people)
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+
+  res.status(401).send("Please Provide Credentials");
 });
-app.get("/about", (req, res) => {
-  res.send("about page");
-});
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resources not found</h1>");
-});
+
+
+
 app.listen(5000, () => {
-  console.log("server is listening on port 5000");
+  console.log("Server is listening on port 5000....");
 });
